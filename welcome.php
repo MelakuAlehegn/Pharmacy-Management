248f93fc5dbeb201175bb8f1365f480a16d50403
teddycheru <80104928+teddycheru@users.noreply.github.com>
2022-06-07 09:44:18 +0300

    Landing page. The HTML part need to be workedout

---
+++ welcome.php
@@ -0,0 +1,30 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="icon" href="images/logo.jpg">
+    <link rel="stylesheet" href="css/style-index.css">
+</head>
+<body>
+    
+
+
+</body>
+
+</html>
